9fbccca56f407f026dc6e15c1fbbe1bdeae97c7b
Maksim Berdnikov <presslxqii@yandex.ru>
2023-02-11 18:14:51 +0300

    changed project

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,35 @@
-// src/App.tsx
-import * as React from 'react'
-import ReactDOM from 'react-dom/client';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
-// App Component
-const App = () => (<div>
-    <h1>Hello, ESBUILD!</h1>
-    <Panel />
-</div>)
+function App() {
+  const [count, setCount] = useState(0)
 
-const Panel = () => <h2>I'm a Panel TES 1T</h2>
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
 
-// Mount component
-const root = ReactDOM.createRoot(
-        document.getElementById('root')
-);
-root.render(<App />);
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
